0eb586f808f00c084ef50483c4e06c81e489e2f9
Bradley Boehmke <bradleyboehmke@gmail.com>
2016-08-17 15:37:22 -0400

    Create 01-intro-students.R

---
+++ oper200/introduction/01-intro-students.R
@@ -0,0 +1,100 @@
+# Session 1: An introduction to statistical programming
+
+# Place a hashtag ("#") at the beginning of any line that is just a note; this
+# will keep R from running these lines as code. Any lines without a hashtag
+# will be assessed when sourced
+
+2 + 2
+
+################
+# Getting Help #
+################
+# Notes: 
+
+
+
+
+######################
+# Set Your Directory #
+######################
+# Notes: 
+
+
+
+
+#####################
+# R as a Calculator #
+#####################
+# Notes: 
+
+
+
+
+##################
+# Simple Objects #
+##################
+# Notes: 
+
+
+
+##############
+# YOUR TURN! #
+##############
+# Calculate Q in the in the economic order quantity model presented
+
+
+
+
+
+#########################
+# Workspace Environment #
+#########################
+# Notes: 
+
+
+
+
+###########
+# Vectors #
+###########
+# Notes: 
+
+
+
+
+#################
+# Vectorization #
+#################
+# Notes:
+
+
+
+
+##############
+# YOUR TURN! #
+##############
+# Calculate Q in the in the economic order quantity model presented
+
+
+
+
+#########################
+# Working with Packages #
+#########################
+# Notes:
+
+
+
+
+##############
+# YOUR TURN! #
+##############
+# Download the packages listed on the slide
+
+
+
+
+##################
+# Using Packages #
+##################
+# Notes:
